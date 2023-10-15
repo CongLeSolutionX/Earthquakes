aaabc339c2e062be20270965c40fac923253858f
CongLe <longchike@gmail.com>
2023-10-15 11:57:04 -0700

    - Endpoint for different type of queries - Support 2 types of fetches ( not done)

--- Earthquakes/MVVM-Examples/Google-Books/Books.swift
+++ Earthquakes/MVVM-Examples/Google-Books/Books.swift
@@ -16,14 +16,51 @@ enum BooksServiceError: Error {
     case decodingError(message: String)
 }
 
-// MARK: - Query
+// MARK: - Query Endpoints
+
+enum Endpoint {
+    case volumes(queries: [BooksQuery])
+    case volumeDetail(id: String)
+    // add here other endpoints if needed
+    
+    var url: URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "www.googleapis.com"
+        components.path = self.path
+        components.queryItems = self.queryItems
+        
+        return components.url
+    }
+    
+    private var path: String {
+        switch self {
+        case .volumes:
+            /// https://www.googleapis.com/books/v1/volumes
+            return "/books/v1/volumes"
+        case .volumeDetail(let id):
+            /// https://www.googleapis.com/books/v1/volumes/{volumeId}
+            return "/books/v1/volumes/\(id)"
+        }
+    }
+    
+    var queryItems: [URLQueryItem]? {
+        switch self {
+        case .volumes(let queries):
+            /// https://www.googleapis.com/books/v1/volumes?q={ queries }
+            return queries.map { $0.queryItem }
+        default:
+            return nil
+        }
+    }
+}
 enum BooksQuery {
     case query(String)
     case maxResults(Int)
     case startIndex(Int)
     case printType(String)
     case langRestrict(String)
-
+    
     var queryItem: URLQueryItem {
         switch self {
         case .query(let string): return URLQueryItem(name: "q", value: string)
@@ -36,7 +73,6 @@ enum BooksQuery {
 }
 
 // MARK: - SERVICES
-///  https://www.googleapis.com/books/v1/volumes?q=time&printType=magazines&key=AIzaSyDn3G9HAKGl07eN3-N0f_51NImwxszHbS0
 class BooksService {
     private let session: URLSession = {
         let config = URLSessionConfiguration.default
@@ -50,19 +86,25 @@ class BooksService {
         return decoder
     }()
     
-    func fetchBooks(queries: [BooksQuery], completion: @escaping (Result<[Item], Error>) -> Void) {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = "www.googleapis.com"
-        urlComponents.path = "/books/v1/volumes"
-        urlComponents.queryItems = queries.map { $0.queryItem }
-            
-        guard let url = urlComponents.url else {
+    func fetchBooks(queries: [BooksQuery], completion: @escaping (Result<BookResponse, Error>) -> Void) {
+        /// URL link: https://www.googleapis.com/books/v1/volumes?q=harry%20potter&maxResults=10&startIndex=2
+        guard let url = Endpoint.volumes(queries: queries).url else {
             DispatchQueue.main.async { completion(.failure(BooksServiceError.invalidURL)) }
             return
         }
-        
-        session.dataTask(with: url) { (data, response, error) in
+        performRequest(url: url, completion: completion)
+    }
+    
+    //    func fetchBookDetail(id: String, completion: @escaping (Result<BookDetailResponse, Error>) -> Void) {
+    //        guard let url = Endpoint.volumeDetail(id: id).url else {
+    //            DispatchQueue.main.async { completion(.failure(BooksServiceError.invalidURL)) }
+    //            return
+    //        }
+    //        performRequest(url: url, completion: completion)
+    //    }
+    
+    private func performRequest<T: Decodable>(url: URL, completion: @escaping (Result<T, Error>) -> Void) {
+        session.dataTask(with: url) { (data, _, error) in
             DispatchQueue.main.async {
                 if let error = error {
                     completion(.failure(BooksServiceError.responseError(message: error.localizedDescription)))
@@ -73,8 +115,8 @@ class BooksService {
                     return
                 }
                 do {
-                    let response = try self.decoder.decode(BookResponse.self, from: data)
-                    completion(.success(response.items))
+                    let response = try self.decoder.decode(T.self, from: data)
+                    completion(.success(response))
                 } catch {
                     completion(.failure(BooksServiceError.decodingError(message: error.localizedDescription)))
                 }
@@ -92,12 +134,14 @@ class BooksViewModel: ObservableObject {
     init(booksService: BooksService = BooksService()) {
         self.booksService = booksService
     }
-    
+    func fetchBooks(query: String) {
+        
+    }
     func fetchBooks() {
         booksService.fetchBooks(queries: [.query("harry potter"), .maxResults(10), .startIndex(2)]) { result in
             switch result {
-            case .success(let books):
-                self.books = books
+            case .success(let bookResponse):
+                self.books = bookResponse.items
             case .failure(let error):
                 self.error = error as? BooksServiceError
             }
@@ -108,41 +152,57 @@ class BooksViewModel: ObservableObject {
 // MARK: - VIEW
 struct BooksView: View {
     @ObservedObject private var booksViewModel = BooksViewModel()
+    @State private var searchQuery = ""
     
     var body: some View {
-        VStack {
-            if let error = booksViewModel.error {
-                handleError(error: error)
-            }
-            List(booksViewModel.books) { book in
-                VStack(alignment: .leading) {
-                    Text(book.volumeInfo.title)
-                        .font(.headline)
-                    
-                    Text(book.volumeInfo.authors.first ?? "Anonymous")
-                        .font(.subheadline)
-                    
-                    Text(book.volumeInfo.publishedDate)
-                        .font(.subheadline)
-                    
-                    Text(book.saleInfo.saleability)
-                        .font(.callout)
-                    
-                    Divider()
-                    
-                    Text(book.searchInfo.textSnippet)
-                        .font(.caption2)
-                    
-                    Divider()
-                    
-                    Text(book.volumeInfo.description)
-                        .font(.caption)
-                    
-                    Spacer()
+        NavigationView {
+            VStack {
+                TextField("Search...", text: $searchQuery)
+                    .padding(7)
+                    .background(Color(.systemGray6))
+                    .cornerRadius(8)
+                    .padding(.horizontal, 10)
+                
+                List(booksViewModel.books) { book in
+                    VStack(alignment: .leading) {
+                        Text(book.volumeInfo.title)
+                            .font(.headline)
+                        
+                        Text(book.volumeInfo.authors.first ?? "Anonymous")
+                            .font(.subheadline)
+                        
+                        Text(book.volumeInfo.publishedDate)
+                            .font(.subheadline)
+                        
+                        Text(book.saleInfo.saleability)
+                            .font(.callout)
+                        
+                        Divider()
+                        
+                        Text(book.searchInfo.textSnippet)
+                            .font(.caption2)
+                        
+                        Divider()
+                        
+                        Text(book.volumeInfo.description)
+                            .font(.caption)
+                        
+                        Spacer()
+                    }
+                }//List
+                .onAppear {
+                    if self.searchQuery.isEmpty {
+                        self.booksViewModel.fetchBooks() // default resultss
+                    } else {
+                        self.booksViewModel.fetchBooks(query: self.searchQuery) // use user inputs as queries
+                    }
                 }
-            }.onAppear {
-                self.booksViewModel.fetchBooks()
+                
+                if let error = booksViewModel.error {
+                    handleError(error: error)
+                }
             }
+            .navigationBarTitle("Google Books")
         }
     }
     
